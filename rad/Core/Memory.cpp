39e0a74eabf39b43b807f906aa1aa94b09414f82
Ma, Jerry <majunlichn@outlook.com>
2023-05-09 23:32:41 +0800

    rad/Core/Memory

---
+++ rad/Core/Memory.cpp
@@ -0,0 +1,5 @@
+#include "rad/Core/Memory.h"
+
+NAMESPACE_RAD_BEGIN
+
+NAMESPACE_RAD_END
---
+++ rad/Core/Memory.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "rad/Core/Common.h"
+#include <memory>
+
+NAMESPACE_RAD_BEGIN
+
+NAMESPACE_RAD_END
