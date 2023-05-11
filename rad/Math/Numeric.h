a8aa143eed6694ca13851252cac5834c5d1c2b92
Ma, Jerry <majunlichn@outlook.com>
2023-05-11 15:54:42 +0800

    rad/Math/Numeric

--- rad/Math/Numeric.h
+++ rad/Math/Numeric.h
@@ -3,9 +3,9 @@
 #include "rad/Core/Config.h"
 #include <cmath>
 #include <numbers>
-#include <complex>
-#include <valarray>
 #include <numeric>
+
+NAMESPACE_RAD_BEGIN
 
 // https://www.pbr-book.org/3ed-2018/Shapes/Managing_Rounding_Error#x1-ErrorPropagation
 template<typename Float>
@@ -14,3 +14,5 @@ constexpr Float GetFloatingErrorGamma(int n)
     const Float MachineEpsilon = std::numeric_limits<Float>::epsilon() * 0.5f;
     return (n * MachineEpsilon) / (1 - n * MachineEpsilon);
 }
+
+NAMESPACE_RAD_END
