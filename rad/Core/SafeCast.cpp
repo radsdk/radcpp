2d1917be2cba9132f4d634d28d7ba776a4757f8c
Jerry Ma <majunlichn@outlook.com>
2024-06-09 14:45:51 +0800

    rad/Core/SafeCast

---
+++ rad/Core/SafeCast.cpp
@@ -0,0 +1,6 @@
+#include "SafeCast.h"
+
+namespace rad
+{
+
+} // namespace rad
---
+++ rad/Core/SafeCast.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <boost/numeric/conversion/cast.hpp>
+
+namespace rad
+{
+
+using boost::numeric_cast;
+
+using boost::numeric::bad_numeric_cast;
+using boost::numeric::positive_overflow;
+using boost::numeric::negative_overflow;
+
+} // namespace rad
