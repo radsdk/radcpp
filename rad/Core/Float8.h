e7d29c910d69faf1fa1c6d2e7c5c5a699cb5975c
Jerry Ma <majunlichn@outlook.com>
2024-06-09 18:56:35 +0800

    rad/Core/Float8

---
+++ rad/Core/Float8.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Platform.h"
+#include "Float.h"
+
+namespace rad
+{
+
+uint8_t FP8E4M3_FromFP32(float f);
+float FP8E4M3_ToFP32(uint8_t input);
+
+uint8_t FP8E5M2_FromFP32(float f);
+float FP8E5M2_ToFP32(uint8_t input);
+
+} // namespace rad
