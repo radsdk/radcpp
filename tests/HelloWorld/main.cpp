401a6f5a95a084d29e71581ba022a97fa76b2df4
Jerry Ma <majunlichn@outlook.com>
2024-06-11 12:05:47 +0800

    rad/System/CpuInfo

--- tests/HelloWorld/main.cpp
+++ tests/HelloWorld/main.cpp
@@ -1,5 +1,7 @@
 #include "rad/Core/Platform.h"
+#include "rad/Core/String.h"
 #include "rad/IO/Logging.h"
+#include "rad/System/CpuInfo.h"
 #include <gtest/gtest.h>
 
 int main(int argc, char* argv[])
@@ -7,6 +9,11 @@ int main(int argc, char* argv[])
     rad::SetupDefaultLogger("HelloWorld.log", true);
 
     SPDLOG_INFO("Hello, {}!", "World");
+#if defined(CPU_FEATURES_ARCH_X86)
+    SPDLOG_INFO("CPU: {} ({})",
+        rad::StrTrim(rad::g_X86Info.brand_string),
+        rad::g_X86Info.vendor);
+#endif
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
