60daccf54e8a125a1b4bcdfe816d3e94f8813fe6
Jerry Ma <majunlichn@outlook.com>
2024-06-13 17:52:56 +0800

    rad/Gui/Application: DisplayInfo

--- rad/Gui/Application.cpp
+++ rad/Gui/Application.cpp
@@ -28,7 +28,10 @@ bool Application::Init(int argc, char** argv)
         RAD_LOG(Gui, info, "SDL initialized on {}: {}.{}.{} ({})", SDL_GetPlatform(),
             SDL_VERSIONNUM_MAJOR(version), SDL_VERSIONNUM_MINOR(version), SDL_VERSIONNUM_MICRO(version),
             SDL_GetRevision());
-        RAD_LOG(Gui, info, "Working directory: {}", SDL_GetBasePath());
+        RAD_LOG(Gui, info, "Working Directory: {}", SDL_GetBasePath());
+        RAD_LOG(Gui, info, "Video Driver: {}", SDL_GetCurrentVideoDriver());
+        RAD_LOG(Gui, info, "Audio Driver: {}", SDL_GetCurrentAudioDriver());
+        UpdateDisplayInfos();
         return true;
     }
     else
@@ -53,6 +56,62 @@ bool Application::IsInitialized(SDL_InitFlags flags)
 {
     SDL_InitFlags res = SDL_WasInit(flags);
     return HasBits(flags, res);
+}
+
+void Application::UpdateDisplayInfos()
+{
+    int displayCount = 0;
+    SDL_DisplayID* ids = SDL_GetDisplays(&displayCount);
+    m_displays.clear();
+    m_displays.resize(displayCount);
+    for (int i = 0; i < displayCount; ++i)
+    {
+        DisplayInfo info = {};
+        SDL_DisplayID id = ids[i];
+        info.id = id;
+        info.name = SDL_GetDisplayName(id);
+
+        if (SDL_GetDisplayBounds(id, &info.bounds) != 0)
+        {
+            RAD_LOG(Gui, err, "SDL_GetDisplayBounds failed: {}", SDL_GetError());
+        }
+        if (SDL_GetDisplayUsableBounds(id, &info.usableBounds) != 0)
+        {
+            RAD_LOG(Gui, err, "SDL_GetDisplayUsableBounds failed: {}", SDL_GetError());
+        }
+
+        info.naturalOrientation = SDL_GetNaturalDisplayOrientation(id);
+        info.currentOrientation = SDL_GetCurrentDisplayOrientation(id);
+
+        info.scale = SDL_GetDisplayContentScale(id);
+        if (info.scale == 0.0f)
+        {
+            RAD_LOG(Gui, err, "SDL_GetDisplayContentScale failed: {}", SDL_GetError());
+        }
+
+        info.propID = SDL_GetDisplayProperties(id);
+        if (info.propID == 0)
+        {
+            info.hdrEnabled = SDL_GetBooleanProperty(info.propID, SDL_PROP_DISPLAY_HDR_ENABLED_BOOLEAN, SDL_FALSE);
+            info.sdrWhitePoint = SDL_GetFloatProperty(info.propID, SDL_PROP_DISPLAY_SDR_WHITE_POINT_FLOAT, 1.0f);
+            info.hdrHeadroom = SDL_GetFloatProperty(info.propID, SDL_PROP_DISPLAY_HDR_HEADROOM_FLOAT, 1.0f);
+            info.kmsdrmOrientation = SDL_GetNumberProperty(info.propID, SDL_PROP_DISPLAY_KMSDRM_PANEL_ORIENTATION_NUMBER, 0);
+        }
+        else
+        {
+            RAD_LOG(Gui, err, "SDL_GetDisplayProperties failed: {}", SDL_GetError());
+        }
+
+        int count = 0;
+        const SDL_DisplayMode** modes = SDL_GetFullscreenDisplayModes(id, &count);
+        if (count > 0)
+        {
+            info.modes.resize(count);
+            std::memcpy(info.modes.data(), modes, count * sizeof(modes[0]));
+        }
+        info.desktopMode = SDL_GetDesktopDisplayMode(id);
+        info.currentMode = SDL_GetCurrentDisplayMode(id);
+    }
 }
 
 } // namespace rad
--- rad/Gui/Application.h
+++ rad/Gui/Application.h
@@ -11,6 +11,31 @@ namespace rad
 
 spdlog::logger* GetGuiLogger();
 
+struct DisplayInfo
+{
+    SDL_DisplayID id;
+    const char* name;
+    SDL_Rect bounds;
+    // This is the same area as bounds, but with portions reserved by the system removed.
+    SDL_Rect usableBounds;
+    SDL_DisplayOrientation naturalOrientation;
+    SDL_DisplayOrientation currentOrientation;
+    // The content scale is the expected scale for content based on the DPI settings of the display.
+    float scale;
+
+    // https://wiki.libsdl.org/SDL3/SDL_GetDisplayProperties
+    SDL_PropertiesID propID;
+    SDL_bool hdrEnabled;
+    float sdrWhitePoint;
+    float hdrHeadroom;
+    Sint64 kmsdrmOrientation;
+
+    std::vector<const SDL_DisplayMode*> modes;
+    const SDL_DisplayMode* desktopMode;
+    const SDL_DisplayMode* currentMode;
+
+}; // struct DisplayInfo
+
 class Application
 {
 public:
@@ -23,7 +48,11 @@ public:
     SDL_InitFlags GetInitialized();
     bool IsInitialized(SDL_InitFlags flags);
 
+    const std::vector<DisplayInfo>& GetDisplayInfos() { return m_displays; }
+
 private:
+    void UpdateDisplayInfos();
+    std::vector<DisplayInfo> m_displays;
 
 }; // class Application
 
