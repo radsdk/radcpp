14c672edd9192cfa746bf92aa3b047499e97a2d5
Jerry Ma <majunlichn@outlook.com>
2024-07-01 12:46:26 +0800

    setup: fix script

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def build_SDL():
     pushd("imported")
     build_dir = script_root + "/imported/build/SDL"
     install_dir = script_root + "/imported/installed/SDL"
-    shell(f"cmake -S . -B \"{build_dir}\" -DCMAKE_INSTALL_PREFIX=\"{install_dir}\"")
+    shell(f"cmake -S SDL -B \"{build_dir}\" -DCMAKE_INSTALL_PREFIX=\"{install_dir}\"")
     shell(f"cmake --build \"{build_dir}\" --target install --config Release")
     popd()
 
@@ -43,7 +43,7 @@ def build_SDL_mixer():
     build_dir = script_root + "/imported/build/SDL_mixer"
     install_dir = script_root + "/imported/installed/SDL_mixer"
     sdl3_dir = script_root + "/imported/installed/SDL"
-    shell(f"cmake -S . -B \"{build_dir}\" -DCMAKE_INSTALL_PREFIX=\"{install_dir}\"",
+    shell(f"cmake -S SDL_mixer -B \"{build_dir}\" -DCMAKE_INSTALL_PREFIX=\"{install_dir}\"",
                    env=dict(os.environ, SDL3_DIR=sdl3_dir))
     shell(f"cmake --build \"{build_dir}\" --target install --config Release")
     popd() # root
@@ -56,9 +56,11 @@ def generate_project_files():
 
 def main() -> int:
     try:
+        pushd(script_root)
         build_SDL()
         build_SDL_mixer()
         generate_project_files()
+        popd()
         return 0
     except Exception as e:
         print(e)
