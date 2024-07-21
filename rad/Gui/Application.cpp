63b431d8e49333dbdf6d42f3f303186e1f92d65a
Jerry Ma <majunlichn@outlook.com>
2024-07-22 01:05:26 +0800

    rad/Gui/Application

--- rad/Gui/Application.cpp
+++ rad/Gui/Application.cpp
@@ -406,4 +406,93 @@ void Application::UpdateDisplayInfos()
     }
 }
 
+Uint64 GetPerfCounter()
+{
+    return SDL_GetPerformanceCounter();
+}
+
+Uint64 GetPerfCounterFrequency()
+{
+    return SDL_GetPerformanceFrequency();
+}
+
+void DelayMS(Uint32 ms)
+{
+    SDL_Delay(ms);
+}
+
+void DelayNS(Uint64 ns)
+{
+    SDL_DelayNS(ns);
+}
+
+namespace sdl
+{
+
+Timer::Timer()
+{
+}
+
+Timer::~Timer()
+{
+}
+
+bool Timer::StartMS(Uint32 interval, TimerCallbackMS callback, void* userData)
+{
+    assert(m_id == 0);
+    m_id = SDL_AddTimer(interval, callback, userData);
+    if (m_id != 0)
+    {
+        return true;
+    }
+    else
+    {
+        RAD_LOG(GetGuiLogger(), err, "SDL_AddTimer failed: {}", SDL_GetError());
+        return false;
+    }
+}
+
+bool Timer::StartNS(Uint64 interval, TimerCallbackNS callback, void* userData)
+{
+    assert(m_id == 0);
+    m_id = SDL_AddTimerNS(interval, callback, userData);
+    if (m_id != 0)
+    {
+        return true;
+    }
+    else
+    {
+        RAD_LOG(GetGuiLogger(), err, "SDL_AddTimerNS failed: {}", SDL_GetError());
+        return false;
+    }
+}
+
+void Timer::Stop()
+{
+    if (m_id != 0)
+    {
+        if (SDL_RemoveTimer(m_id) != 0)
+        {
+            RAD_LOG(GetGuiLogger(), err, "SDL_RemoveTimer failed: {}", SDL_GetError());
+        }
+        m_id = 0;
+    }
+}
+
+bool TimerTaskMS::Start(Uint32 interval)
+{
+    return Timer::StartMS(interval, [](void* userData, SDL_TimerID timerID, Uint32 interval)->Uint32 {
+        return reinterpret_cast<TimerTaskMS*>(userData)->Callback(timerID, interval);
+        }, this);
+}
+
+bool TimerTaskNS::Start(Uint64 interval)
+{
+    return Timer::StartNS(interval, [](void* userData, SDL_TimerID timerID, Uint64 interval)->Uint64 {
+        return reinterpret_cast<TimerTaskNS*>(userData)->Callback(timerID, interval);
+        }, this);
+}
+
+} // namespace sdl
+
 } // namespace rad
--- rad/Gui/Application.h
+++ rad/Gui/Application.h
@@ -106,4 +106,56 @@ private:
 
 Application* GetApp();
 
+// Get the current value of the high resolution counter.
+Uint64 GetPerfCounter();
+// Get the frequency in Hz of the high resolution counter.
+Uint64 GetPerfCounterFrequency();
+
+// Wait a specified number of milliseconds before returning.
+void DelayMS(Uint32 ms);
+// Wait a specified number of nanoseconds before returning.
+void DelayNS(Uint64 ns);
+
+namespace sdl
+{
+
+using TimerCallbackMS = SDL_TimerCallback;
+using TimerCallbackNS = SDL_NSTimerCallback;
+
+class Timer : public rad::RefCounted<Timer>
+{
+public:
+    Timer();
+    virtual ~Timer();
+
+    bool StartMS(Uint32 interval, TimerCallbackMS callback, void* userData);
+    bool StartNS(Uint64 interval, TimerCallbackNS callback, void* userData);
+    void Stop();
+
+    SDL_TimerID GetID() const { return m_id; }
+
+protected:
+    SDL_TimerID m_id = 0;
+}; // class Timer
+
+class TimerTaskMS : public Timer
+{
+public:
+    // SDL_TimerCallback
+    virtual Uint32 Callback(SDL_TimerID timerID, Uint32 interval) = 0;
+    bool Start(Uint32 interval);
+
+}; // class TimerTaskMS
+
+class TimerTaskNS : public Timer
+{
+public:
+    // SDL_NSTimerCallback
+    virtual Uint64 Callback(SDL_TimerID timerID, Uint64 interval) = 0;
+    bool Start(Uint64 interval);
+
+}; // class TimerTaskMS
+
+} // namespace sdl
+
 } // namespace rad
